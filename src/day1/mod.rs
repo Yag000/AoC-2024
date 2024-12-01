95f6fe747803ca8992e11f732fea2b41a2e31ed3
Yago Iglesias <me@yagoiglesias.fr>
2024-12-01 10:23:08 +0100

    day 1

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,11 +1,61 @@
+use std::collections::HashMap;
+
 pub fn part1() -> String {
     let path = "input/day1.txt";
     let input = std::fs::read_to_string(path).unwrap();
-    "".to_string()
+
+    let lists: Vec<(i32, i32)> = input
+        .lines()
+        .map(|line| {
+            let v: Vec<&str> = line.split("   ").collect();
+            (v[0].parse().unwrap(), v[1].parse().unwrap())
+        })
+        .collect();
+
+    let (mut list_a, mut list_b): (Vec<i32>, Vec<i32>) = lists.into_iter().unzip();
+    list_a.sort();
+    list_b.sort();
+
+    let couple = list_a.into_iter().zip(list_b.into_iter());
+
+    couple
+        .into_iter()
+        .map(|(a, b)| ((a - b).abs()))
+        .sum::<i32>()
+        .to_string()
 }
 
 pub fn part2() -> String {
     let path = "input/day1.txt";
     let input = std::fs::read_to_string(path).unwrap();
-    "".to_string()
+
+    let lists: Vec<(i32, i32)> = input
+        .lines()
+        .map(|line| {
+            let v: Vec<&str> = line.split("   ").collect();
+            (v[0].parse().unwrap(), v[1].parse().unwrap())
+        })
+        .collect();
+
+    let (list_a, list_b): (Vec<i32>, Vec<i32>) = lists.into_iter().unzip();
+
+    let mut hash: HashMap<i32, i32> = HashMap::new();
+
+    for i in list_a {
+        let occ = list_b
+            .clone()
+            .into_iter()
+            .filter(|v| *v == i)
+            .fold(0, |acc, _| acc + 1);
+
+        let _ = hash.insert(i, occ);
+    }
+
+    let couple = hash.keys().zip(hash.values());
+
+    couple
+        .into_iter()
+        .map(|(a, b)| a * b)
+        .sum::<i32>()
+        .to_string()
 }
