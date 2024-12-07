9c9a40798c32520c534e8e629dab06a24b53ff9f
Yago Iglesias <me@yagoiglesias.fr>
2024-12-07 14:41:28 +0100

    feat: day 6 part 2

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+#[derive(Copy, Clone, PartialEq)]
 enum Direction {
     Up,
     Down,
@@ -30,12 +33,12 @@ fn next_pos(p: (i32, i32), d: &Direction) -> (i32, i32) {
     (x + dx, y + dy)
 }
 
-fn is_inside(t: &[Vec<Option<i32>>], p: (i32, i32)) -> bool {
+fn is_inside(t: &[Vec<Option<bool>>], p: (i32, i32)) -> bool {
     let (x, y) = p;
     y >= 0 && y < t.len() as i32 && x >= 0 && x < t[y as usize].len() as i32
 }
 
-fn is_wall(t: &[Vec<Option<i32>>], p: (i32, i32)) -> bool {
+fn is_wall(t: &[Vec<Option<bool>>], p: (i32, i32)) -> bool {
     let (x, y) = p;
     is_inside(t, p) && t[x as usize][y as usize].is_none()
 }
@@ -44,17 +47,17 @@ pub fn part1() -> String {
     let path = "input/day6.txt";
     let input = std::fs::read_to_string(path).unwrap();
 
-    let mut board: Vec<Vec<Option<i32>>> = input
+    let mut board: Vec<Vec<Option<bool>>> = input
         .lines()
         .map(|line| {
             line.chars()
                 .map(|c| {
                     if c == '.' {
-                        Some(0)
+                        Some(false)
                     } else if c == '#' {
                         None
                     } else if c == '^' {
-                        Some(1)
+                        Some(true)
                     } else {
                         unreachable!()
                     }
@@ -67,7 +70,7 @@ pub fn part1() -> String {
 
     for i in 0..board.len() {
         for j in 0..board[0].len() {
-            if board[i][j] == Some(1) {
+            if board[i][j] == Some(true) {
                 pos = (i as i32, j as i32);
                 break;
             }
@@ -81,7 +84,7 @@ pub fn part1() -> String {
         if is_wall(&board, n) {
             d = turn(d);
         } else {
-            board[pos.0 as usize][pos.1 as usize] = Some(1);
+            board[pos.0 as usize][pos.1 as usize] = Some(true);
             pos = n;
         }
     }
@@ -89,14 +92,101 @@ pub fn part1() -> String {
     board
         .iter()
         .flatten()
-        .filter_map(|v| *v)
+        .filter_map(|v| match v {
+            Some(true) => Some(1),
+            _ => None,
+        })
         .sum::<i32>()
         .to_string()
+}
+
+fn update(h: &mut HashMap<(i32, i32), Vec<Direction>>, d: Direction, p: (i32, i32)) {
+    match h.get_mut(&p) {
+        None => {
+            h.insert(p, vec![d]);
+        }
+        Some(s) => s.push(d),
+    }
+}
+
+fn loops(t: &[Vec<Option<bool>>], p: (i32, i32)) -> bool {
+    let mut pos = p;
+
+    let mut d = Direction::Up;
+
+    let mut board = t.to_owned();
+
+    let mut h: HashMap<(i32, i32), Vec<Direction>> = HashMap::new();
+
+    while is_inside(&board, pos) {
+        let n = next_pos(pos, &d);
+
+        if board[pos.0 as usize][pos.1 as usize] == Some(true) {
+            if let Some(v) = h.get(&pos) {
+                if v.contains(&d) {
+                    return true;
+                }
+            }
+        }
+
+        if is_wall(&board, n) {
+            d = turn(d);
+        } else {
+            board[pos.0 as usize][pos.1 as usize] = Some(true);
+            update(&mut h, d, pos);
+            pos = n;
+        }
+    }
+
+    false
 }
 
 pub fn part2() -> String {
     let path = "input/day6.txt";
     let input = std::fs::read_to_string(path).unwrap();
 
-    "".to_string()
+    let mut board: Vec<Vec<Option<bool>>> = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| {
+                    if c == '.' {
+                        Some(false)
+                    } else if c == '#' {
+                        None
+                    } else if c == '^' {
+                        Some(true)
+                    } else {
+                        unreachable!()
+                    }
+                })
+                .collect()
+        })
+        .collect();
+
+    let mut pos = (0, 0);
+
+    for i in 0..board.len() {
+        for j in 0..board[0].len() {
+            if board[i][j] == Some(true) {
+                pos = (i as i32, j as i32);
+                break;
+            }
+        }
+    }
+
+    let mut counter = 0;
+    for i in 0..board.len() {
+        for j in 0..board[0].len() {
+            if board[i][j] == Some(false) {
+                board[i][j] = None;
+                if loops(&board, pos) {
+                    counter += 1;
+                }
+                board[i][j] = Some(false);
+            }
+        }
+    }
+
+    counter.to_string()
 }
