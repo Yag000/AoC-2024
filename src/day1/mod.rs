41f399a92e67679b9bfc4e9665bb8d82a1cef46a
Yago Iglesias <me@yagoiglesias.fr>
2024-12-11 01:12:28 +0100

    chore: refactor day 1

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -4,22 +4,20 @@ pub fn part1() -> String {
     let path = "input/day1.txt";
     let input = std::fs::read_to_string(path).unwrap();
 
-    let lists: Vec<(i32, i32)> = input
+    let (mut list_a, mut list_b): (Vec<i32>, Vec<i32>) = input
         .lines()
         .map(|line| {
             let v: Vec<&str> = line.split("   ").collect();
-            (v[0].parse().unwrap(), v[1].parse().unwrap())
+            (v[0].parse::<i32>().unwrap(), v[1].parse::<i32>().unwrap())
         })
-        .collect();
+        .unzip();
 
-    let (mut list_a, mut list_b): (Vec<i32>, Vec<i32>) = lists.into_iter().unzip();
     list_a.sort();
     list_b.sort();
 
-    let couple = list_a.into_iter().zip(list_b);
-
-    couple
+    list_a
         .into_iter()
+        .zip(list_b)
         .map(|(a, b)| ((a - b).abs()))
         .sum::<i32>()
         .to_string()
@@ -29,33 +27,21 @@ pub fn part2() -> String {
     let path = "input/day1.txt";
     let input = std::fs::read_to_string(path).unwrap();
 
-    let lists: Vec<(i32, i32)> = input
+    let (list_a, list_b): (Vec<i32>, Vec<i32>) = input
         .lines()
         .map(|line| {
             let v: Vec<&str> = line.split("   ").collect();
-            (v[0].parse().unwrap(), v[1].parse().unwrap())
+            (v[0].parse::<i32>().unwrap(), v[1].parse::<i32>().unwrap())
         })
-        .collect();
-
-    let (list_a, list_b): (Vec<i32>, Vec<i32>) = lists.into_iter().unzip();
+        .unzip();
 
     let mut hash: HashMap<i32, i32> = HashMap::new();
 
     for i in list_a {
-        let occ = list_b
-            .clone()
-            .into_iter()
-            .filter(|v| *v == i)
-            .fold(0, |acc, _| acc + 1);
+        let occ = list_b.iter().filter(|v| **v == i).count();
 
-        let _ = hash.insert(i, occ);
+        hash.insert(i, occ as i32);
     }
 
-    let couple = hash.keys().zip(hash.values());
-
-    couple
-        .into_iter()
-        .map(|(a, b)| a * b)
-        .sum::<i32>()
-        .to_string()
+    hash.drain().map(|(a, b)| a * b).sum::<i32>().to_string()
 }
